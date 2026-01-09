8ec5dd3273a112c1243781953c9d57cb8047ece3
Nandi-in <soumyadeep.nd@gmail.com>
2026-01-09 20:36:48 +0530

    Create d6_runner_up.py

---
+++ d6_runner_up.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    
+    max_score = -101
+    runner_up = -101
+    
+    for score in  arr:
+        if score > max_score:
+            runner_up = max_score
+            max_score = score
+        elif max_score > score > runner_up:
+            runner_up = score
+        
+    print(runner_up)
